d1e61c862859731e3109c06f9c76532bf85ff491
rbeebe <rbeebe@fitbit.com>
2017-12-07 10:55:53 -0800

    solved day 6 finally

--- Day 6 - Memory Reallocation-2.py
+++ Day 6 - Memory Reallocation-2.py
@@ -8,7 +8,7 @@ def reallocate(memory, iterations=None, states=None):
     if iterations is None:
         iterations = 0
     if states is None:
-        states = set()
+        states = {}
     while True:
         largest_bank_value = -1
         largest_bank_idx = 0
@@ -30,15 +30,16 @@ def reallocate(memory, iterations=None, states=None):
             bank_redistribution_value = bank_redistribution_value - 1
         # after redistribution, add the current state to our set and increment iterations
         iterations = iterations + 1
-        if str(memory) in states:
+        if str(memory) in states.keys():
             print('Found a duplicated memory state: ' + str(memory))
             print('Required ' + str(iterations) + ' iterations')
+            print('Reappeared after ' + str(iterations - states[str(memory)]) + ' more iterations')
             return(None)
-        states.add(str(memory))
+        states[str(memory)] = iterations
 
 
 # print(str(iterations))
 # print(str(memory))
 
 reallocate(test_banks)
-reallocate(fancy_banks)
+reallocate(fancy_banks) # got 11264. Too high!
